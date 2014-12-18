7b0c044720ab684ad9a679fbbecb42a90546af89
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2014-12-18 14:10:41 +0100

    Copy config to jquery plugin webpack, test how require sort is handled

--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -1,8 +1,15 @@
 var $ = require('jquery');
+var sort = require('./sort');
 
 $.fn.smorgasbord = function () {
 	return this.each(function () {
-		var $table = $(this);
+		var table = this;
+		var $table = $(table);
 
+		function sortTable() {
+			sort(table);
+		}
+
+		sortTable();
 	});
 };
--- webpack.config.jquery.js
+++ webpack.config.jquery.js
@@ -1,18 +1,47 @@
-//var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 var path = require('path');
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+var BannerPlugin = require('webpack/lib/BannerPlugin');
+
 module.exports = {
-    entry: {
-        jquery: './src/jquery-plugin'
-    },
-    output: {
-        path: path.join(__dirname, 'js'),
-        filename: 'jquery.smorgasbord.js',
-        library: ['smorgasbord', '[name]'],
-        libraryTarget: 'umd',
-        runtimePrefix: '',
-        runtimeDivider: ''
-    },
-    externals: {
-        'jquery': 'jQuery'
-    }
+	entry: {
+		jquery: './src/jquery-plugin'
+	},
+	output: {
+		path: path.join(__dirname, 'js'),
+		filename: 'jquery.smorgasbord.js',
+		library: ['smorgasbord', '[name]'],
+		libraryTarget: 'umd',
+		runtimePrefix: '',
+		runtimeDivider: ''
+	},
+	externals: {
+		'jquery': 'jQuery'
+	},
+	plugins: [
+		new UglifyJsPlugin({
+			output: {
+				beautify: true,
+				bracketize: true,
+				width: 160
+			},
+			compress: {
+				sequences: false,
+				conditionals: false,
+				comparisons: false,
+				evaluate: false,
+				booleans: false,
+				loops: false,
+				join_vars: false,
+				cascade: false
+
+			},
+			mangle: false
+		}),
+		new BannerPlugin(
+			'/*jshint -W106, -W062*/\n/*globals module, define, exports*/', {
+				raw: true,
+				entryOnly: true
+			}
+		)
+	]
 };
