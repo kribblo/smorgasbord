b95e6a7fd3b5535be77e046bea55cbd85dc2f461
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2015-03-02 21:44:44 +0100

    Fix util.js

--- src/util.js
+++ src/util.js
@@ -1,34 +1,32 @@
-var util = function () {
-	'use strict';
+var util = {
 
-	return {
-		arrayToUpperCase: function (inArray) {
-			for (var i = 0; i < inArray.length; i++) {
-				inArray[i] = inArray[i].toUpperCase();
-			}
-			return inArray;
-		},
+    arrayToUpperCase: function (inArray) {
+        for (var i = 0; i < inArray.length; i++) {
+            inArray[i] = inArray[i].toUpperCase();
+        }
+        return inArray;
+    },
 
-		makeElement: function (html) {
-			var div = document.createElement('div');
-			div.innerHTML = html;
-			return div.firstChild;
-		},
+    makeElement: function (html) {
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div.firstChild;
+    },
 
-		mergeOptions: function (options, defaultOptions) {
-			options = options || {};
-			defaultOptions = defaultOptions || {};
+    mergeOptions: function (options, defaultOptions) {
+        options = options || {};
+        defaultOptions = defaultOptions || {};
 
-			for (var attr in defaultOptions) {
-				/*jshint -W089 */
-				if (!options.hasOwnProperty(attr)) {
-					options[attr] = defaultOptions[attr];
-				}
-			}
+        for (var attr in defaultOptions) {
+            /*jshint -W089 */
+            if (!options.hasOwnProperty(attr)) {
+                options[attr] = defaultOptions[attr];
+            }
+        }
 
-			return options;
-		}
-	};
+        return options;
+    }
+
 };
 
 module.exports = util;
